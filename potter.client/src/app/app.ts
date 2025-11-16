bc854e43836d54bc58acab813e2a97cd5b08329c
Justin Heath <70263625+justcoder1@users.noreply.github.com>
2025-11-16 20:57:16 +0000

    filtering done (#36)

--- potter.client/src/app/app.ts
+++ potter.client/src/app/app.ts
@@ -1,17 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
-import { IntellectualPropertyAsset } from './types';
 import MOCK_IP_ASSETS from '../temp/mock-ip-assets';
+import { IntellectualPropertyAsset } from './types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css',
-  imports: [MatButtonModule, MatTableModule, CommonModule],
+  imports: [MatButtonModule, CommonModule, MatFormFieldModule, MatInputModule, MatTableModule],
 })
 export class App implements OnInit {
   displayedColumns: (keyof IntellectualPropertyAsset | 'actions')[] = [
@@ -23,12 +26,22 @@ export class App implements OnInit {
     'actions'
   ];
 
-  dataSource: IntellectualPropertyAsset[] = MOCK_IP_ASSETS;
+  dataSource = new MatTableDataSource<IntellectualPropertyAsset>();
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     // this.getForecasts();
+    this.dataSource.data = MOCK_IP_ASSETS;
+
+    this.dataSource.filterPredicate = (data: IntellectualPropertyAsset, filter: string): boolean => {
+      return data.client.toLowerCase().includes(filter);
+    };
+  }
+
+   applyClientFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   // getForecasts() {
